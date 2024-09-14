e1a2a64ff876151ebdc191d54f7aa81b08451b30
Ayushi <ayushi7300@gmail.com>
2024-09-14 22:48:20 +0530

    Adding new file Lab006 from command line

---
+++ src/Sept/ex_13092024/Lab006.java
@@ -0,0 +1,13 @@
+package Sept.ex_13092024;
+
+public class Lab006 {
+    public static void main(String[] args) {
+        double a=34;
+        double b=10;
+        System.out.println("Sum = " + (a+b));
+        System.out.println("Difference = " + (a-b));
+        System.out.println("Multiplication = " + (a*b));
+        System.out.println("Division = " + (a/b));
+        System.out.println("Modulus = " + (a%b));
+    }
+}
