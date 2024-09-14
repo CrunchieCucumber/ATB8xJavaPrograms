3b623528ec6cde412044b035660926310b476149
Ayushi <ayushi7300@gmail.com>
2024-09-14 22:11:05 +0530

    Till - 11092024

---
+++ src/Sept/ex_13092024/Lab005.java
@@ -0,0 +1,11 @@
+package Sept.ex_13092024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        int n=2;
+        for(int i=1;i<=10;i++)
+        {
+            System.out.printf("%d * %d = %d \n", n, i, n * i);
+        }
+    }
+}
